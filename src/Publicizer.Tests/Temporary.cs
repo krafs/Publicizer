746eb17b361ad10c3bab0c958c18d4b5ecc43cc8
krafs <gratiano_facioplegia@aleeas.com>
2022-08-11 09:07:43 +0200

    Rewrite tests to generate projects manually (#26)

---
+++ src/Publicizer.Tests/Temporary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Publicizer.Tests
+{
+    /// <summary>
+    /// Utility class for creating temporary folders.
+    /// </summary>
+    internal static class Temporary
+    {
+        internal static string NewFolder()
+        {
+            string path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+            return Directory.CreateDirectory(path).FullName;
+        }
+    }
+}
