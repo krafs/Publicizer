aa9cf007866c824ba96fd19ca4271c6a428764f5
krafs <gratiano_facioplegia@aleeas.com>
2022-08-15 18:11:33 +0200

    Add option to not publicize compiler generated members

---
+++ src/Publicizer/PublicizerAssemblyContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Publicizer;
+
+internal sealed class PublicizerAssemblyContext
+{
+    internal PublicizerAssemblyContext(string assemblyName)
+    {
+        AssemblyName = assemblyName;
+    }
+
+    internal string AssemblyName { get; }
+    internal bool ExplicitlyPublicizeAssembly { get; set; } = false;
+    internal bool IncludeCompilerGeneratedMembers { get; set; } = true;
+    internal bool ExplicitlyDoNotPublicizeAssembly { get; set; } = false;
+    internal HashSet<string> PublicizeMemberPatterns { get; } = new HashSet<string>();
+    internal HashSet<string> DoNotPublicizeMemberPatterns { get; } = new HashSet<string>();
+}
