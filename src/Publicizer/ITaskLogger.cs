b0599e96516790c2930014663c62084b3030c900
krafs <gratiano_facioplegia@aleeas.com>
2023-01-16 15:58:58 +0100

    Add logging implementation

---
+++ src/Publicizer/ITaskLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace Publicizer;
+
+public interface ITaskLogger
+{
+    public void Error(string message);
+    public void Warning(string message);
+    public void Info(string message);
+    public void Verbose(string message);
+}
---
+++ src/Publicizer/Logger.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Publicizer;
+
+internal sealed class Logger : ITaskLogger, IDisposable
+{
+    private readonly StreamWriter _logFileWriter = StreamWriter.Null;
+    private readonly TaskLoggingHelper _taskLogger;
+    private readonly string _scope;
+
+    private static string Now => DateTime.Now.ToLongTimeString();
+
+    internal Logger(TaskLoggingHelper taskLogger, Stream stream)
+    {
+        _logFileWriter = new StreamWriter(stream)
+        {
+            AutoFlush = true
+        };
+        _taskLogger = taskLogger;
+        _scope = string.Empty;
+    }
+
+    internal Logger(Logger parentLogger, string scope)
+    {
+        _logFileWriter = parentLogger._logFileWriter;
+        _taskLogger = parentLogger._taskLogger;
+        _scope = $" [{scope}]";
+    }
+
+    public void Error(string message)
+    {
+        _taskLogger.LogError(message);
+        Write("ERR", message);
+    }
+
+    public void Warning(string message)
+    {
+        _taskLogger.LogWarning(message);
+        Write("WRN", message);
+    }
+
+    public void Info(string message)
+    {
+        _taskLogger.LogMessage(MessageImportance.Normal, message);
+        Write("INF", message);
+    }
+
+    public void Verbose(string message)
+    {
+        _taskLogger.LogMessage(MessageImportance.Low, message);
+        Write("VRB", message);
+    }
+
+    private void Write(string logLevel, string message)
+    {
+        _logFileWriter.WriteLine($"[{Now} {logLevel}]{_scope} {message}");
+    }
+
+    internal ITaskLogger CreateScope(string assemblyName)
+    {
+        return new Logger(this, assemblyName);
+    }
+
+    public void Dispose()
+    {
+        _logFileWriter.Dispose();
+    }
+}
