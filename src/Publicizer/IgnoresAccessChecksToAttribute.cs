83c33dfb5f9d7b89999779881c7ed7c51147b5af
khyperia <953151+khyperia@users.noreply.github.com>
2025-01-10 00:58:15 +0100

    Ignore CS0436 in IgnoresAccessChecksToAttribute.cs

--- src/Publicizer/IgnoresAccessChecksToAttribute.cs
+++ src/Publicizer/IgnoresAccessChecksToAttribute.cs
@@ -1,4 +1,6 @@
-﻿namespace System.Runtime.CompilerServices
+﻿#pragma warning disable CS0436 // Type conflicts with imported type
+
+namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
     internal sealed class IgnoresAccessChecksToAttribute : Attribute
